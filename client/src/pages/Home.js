943ccdedde909b0e7d630617124eed337a2e6b42
JoshuaRocko <joshua.chirino@gmail.com>
2020-06-04 03:23:04 -0500

    localstorage, login

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import "./styles/Home.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { config } from "@fortawesome/fontawesome-svg-core";
 
 class Home extends React.Component {
   constructor(props) {
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const scraper = require("./scraper");
 const pool = require("./database");
 const puppeteer = require("puppeteer");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+/* SCRAPER */
 let p;
 
 (async () => {
   const browser = await puppeteer.launch();
   p = await browser.newPage();
-  await p.setDefaultTimeout(45000); 
+  await p.setDefaultTimeout(45000);
   p.on("error", (err) => {
     console.log("error happen at the page: ", err);
   });
@@ -26,16 +29,19 @@ app.get("/search/:med", async (req, res) => {
   res.json(data);
 });
 
-app.get("/users", (req, res) => {
-  pool.query("select * from users", (error, result) => {
-    if (error) throw error;
-    res.send(result);
-  });
+app.get("/info/:med", async (req, res) => {
+  const info = await scraper.getInfo(req.params.med, p);
+  res.json(info);
 });
 
-app.post("/create/:email/:username/:pass", (req, res) => {
+/* QUERIES */
+
+app.post("/adduser", (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
   pool.query(
-    `insert into users (username, email, pass) values (lower('${req.params.username}'), lower('${req.params.email}'), MD5('${req.params.pass}'));`,
+    `insert into users (username, email, pass) values (lower('${username}'), lower('${email}'), MD5('${password}'));`,
     (error, result) => {
       if (error) throw error;
       res.send(result);
@@ -55,10 +61,21 @@ app.get("/verifyuser/:username/:email", (req, res) => {
   );
 });
 
-app.get("/info/:med", async (req, res) => {
-  const info = await scraper.getInfo(req.params.med, p);
-  res.json(info);
+app.get("/login/:username", (req, res) => {
+  pool.query(
+    `select * from users where username=lower('${req.params.username}')`,
+    (error, result) => {
+      if (error) throw error;
+      console.log(result);
+      res.send({
+        userExists: result.length > 0,
+        result: result,
+      });
+    }
+  );
 });
+
+/* CONFIG EXPRESS */
 
 const port = 5000;
 
